d404c54ac721c2275d75b21f145e8b56051e6c10
implodingduck <zsek320@gmail.com>
2022-06-06 10:30:07 -0500

    ok trying to get the right request uid

--- mutationwebhook/index.js
+++ mutationwebhook/index.js
@@ -10,7 +10,7 @@ try {
         cert: fs.readFileSync("/ssl/cert.pem"),
         key: fs.readFileSync("/ssl/key.pem"),
     };
-    console.log("loaded certificates from /mnt");
+    console.log("loaded certificates from /ssl");
 } catch {
     console.log("unable to load certs");
 }
@@ -31,11 +31,11 @@ https.createServer(options, (req, res) => {
                 "apiVersion": "admission.k8s.io/v1",
                 "kind": "AdmissionReview",
                 "response": {
-                  "uid": JSON.parse(body).uid,
+                  "uid": JSON.parse(body).request.uid,
                   "allowed": true
                 }
             }
-            console.log(`My resp: ${resp}`)
+            console.log(`My resp: ${JSON.stringify(resp)}`)
             res.end(JSON.stringify(resp));
             
             
