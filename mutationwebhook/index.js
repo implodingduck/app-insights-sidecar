e38570148e1df7d927fa0f0d259207ae8b211bc5
implodingduck <zsek320@gmail.com>
2022-06-06 13:49:48 -0500

    working with making the patch

--- mutationwebhook/index.js
+++ mutationwebhook/index.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 const https = require("https");
+const Buffer = require("buffer");
+const rfc6902 = require('rfc6902')
 
 let options;
 const port = process.env.port || 8443;
@@ -25,6 +27,7 @@ https.createServer(options, (req, res) => {
         });
         req.on("end", () => {
             const jsonBody = JSON.parse(body)
+            const newJsonBody = JSON.parse(body)
             console.log(`hello world! ${body}`)
             console.log("----------")
             console.log(`Namespace: ${JSON.stringify(jsonBody.request.namespace)}`)
@@ -33,26 +36,39 @@ https.createServer(options, (req, res) => {
             console.log("----------")
             console.log(`Containers: ${JSON.stringify(jsonBody.request.object.spec.containers)}`)
 
-            let sidecarJson = {
-                "name":"sidecar-container1",
-                "image":"ghcr.io/implodingduck/app-insights-sidecar:latest",
-                "env":[
-                    {
-                        "name":"APPINSIGHTS_CONNECTION_STRING",
-                        "value": process.env.APPINSIGHTS_CONNECTION_STRING
-                    }
-                ],
-                "resources":{},
-                "volumeMounts":[
-                    {
-                        "name":"var-logs",
-                        "mountPath":"/var/log"
-                    }
-                ],
-                "imagePullPolicy":"Always"
-            }
+            let addSideCar = true
 
-            console.log(`Sidecar Json: ${JSON.stringify(sidecarJson)}`)
+            for(let c of jsonBody.request.object.spec.containers ){
+                if (c.name === 'app-insights-sidecar'){
+                    addSideCar = false
+                }
+            }
+            let jsonPatch = null
+            if (addSideCar){
+                let sidecarJson = {
+                    "name":"app-insights-sidecar",
+                    "image":"ghcr.io/implodingduck/app-insights-sidecar:latest",
+                    "env":[
+                        {
+                            "name":"APPINSIGHTS_CONNECTION_STRING",
+                            "value": process.env.APPINSIGHTS_CONNECTION_STRING
+                        }
+                    ],
+                    "resources":{},
+                    "volumeMounts":[
+                        {
+                            "name":"var-logs",
+                            "mountPath":"/var/log"
+                        }
+                    ],
+                    "imagePullPolicy":"Always"
+                }
+    
+                console.log(`Sidecar Json: ${JSON.stringify(sidecarJson)}`)
+                newJsonBody.request.object.spec.containers.push(sidecarJson)
+
+                jsonPatch = rfc6902.createPatch(jsonBody, newJsonBody)
+            }
             
             res.writeHead(200, { "Content-Type": "application/json" });
             
@@ -63,6 +79,10 @@ https.createServer(options, (req, res) => {
                   "uid": JSON.parse(body).request.uid,
                   "allowed": true
                 }
+            }
+            if (jsonPatch){
+                resp.response.patchType = "JSONPatch"
+                resp.response.patch = Buffer.from(jsonPatch).toString('base64')
             }
             console.log(`My resp: ${JSON.stringify(resp)}`)
             res.end(JSON.stringify(resp));
